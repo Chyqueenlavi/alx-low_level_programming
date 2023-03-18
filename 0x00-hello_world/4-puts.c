0ff931455cbf04739cb0022d8723bd0f64f117f5
Chyqueenlavi <ikemchika98@gmail.com>
2023-03-18 10:07:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,8 +5,8 @@
  *
  * Return: 0 (success)
  */
-int main()
+int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
